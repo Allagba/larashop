e726d8c95de24d3fd1af42125502fc92767bd930
Allagba <109833636+Allagba@users.noreply.github.com>
2023-08-10 17:01:14 -0400

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Product extends Model {
+use HasFactory;
+
+protected $fillable =
+[
+    'name',
+    'slug',
+    'description',
+    'image_name',
+    'price',
+    'sale_price',
+    'created_at',
+    'updated_at'
+
+];
+
+
+}
